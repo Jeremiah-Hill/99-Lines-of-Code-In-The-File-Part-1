a948fd1b16817b73d815eef4024c3c048cc5ee51
Jeremiah-Hill <77300280+Jeremiah-Hill@users.noreply.github.com>
2021-01-20 16:53:25 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-let friends = [
+let friendArray = [
     'Mike', 'Janet', 'Allen', 'Samson', 'Terrance'
 ];
 
-let num = 99
-
-    while (num >= 1) {
-    
-        if (num === 2) {
-            console.log(num + " lines of code in the file, " + num + " lines of code; " + friends + " strikes one out, clears it all out, " + (num - 1) + " line of code in the file.")
-        } else if (num === 1) {
-            console.log(num + " lines of code in the file, " + num + " lines of code; " + friends + " strikes one out, clears it all out, " + (num - 1) + " lines of code in the file.")
-        } else{
-            console.log(num + " lines of code in the file " + num + " lines of code; " + friends + " strikes one out clears it all out, " + (num - 1) + " lines of code in the file.")
+for (let i = 0; i < friendArray.length; i++) {
+    console.log(friendArray[i] + ":"); {
+        for (let j = 99; j >= 1; j--) {
+            if (j === 2) {
+                console.log(" 2 lines of code in the file, 2 lines of code; " + friendArray[i] + " strikes one out, clears it all out, 1 line of code in the file.");
+            } else if (j === 1) {
+                console.log("1 line of code in the file, 1 line of code; " + friendArray[i] + " strikes one out, clears it all out, no more lines of code in the file.");
+            } else {
+                console.log(j + " lines of code in the file, " + j + " lines of code; " + friendArray[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file. ");
+            }
         }
-        num--;
     }
+}
