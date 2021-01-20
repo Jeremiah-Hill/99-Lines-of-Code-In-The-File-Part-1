efaeadbf6a4440a92ab4b37ff6f2e2539194e822
Jeremiah-Hill <77300280+Jeremiah-Hill@users.noreply.github.com>
2021-01-19 23:42:38 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let friends = [
+    'Mike', 'Janet', 'Allen', 'Samson', 'Davis'
+];
+
+let num = 99
+
+    while (num >= 1) {
+    
+        if (num === 2) {
+            console.log(num + " lines of code in the file, " + num + " lines of code; " + friends + " strikes one out, clears it all out, " + (num - 1) + " line of code in the file.")
+        } else if (num === 1) {
+            console.log(num + " lines of code in the file, " + num + " lines of code; " + friends + " strikes one out, clears it all out, " + (num - 1) + " lines of code in the file.")
+        } else{
+            console.log(num + " lines of code in the file " + num + " lines of code; " + friends + " strikes one out clears it all out, " + (num - 1) + " lines of code in the file.")
+        }
+        num--;
+    }
